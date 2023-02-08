32d08b786651bba5f479b40fc5984870f931e1b0
david <davidleon_06@hotmail.com>
2023-02-08 11:39:45 +0100

    Notes, localstorage, some pages

--- my-notes/src/App.tsx
+++ my-notes/src/App.tsx
@@ -1,5 +1,62 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useMemo } from "react";
+
+import { NewNote } from "./pages/NewNote";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
 function App() {
-  return <h1>hi</h1>;
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />;
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>show</h1>} />
+          <Route path="edit" element={<h1>edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
---
+++ my-notes/src/pages/NewNote.tsx
@@ -0,0 +1,10 @@
+import { NoteForm } from "../components/NoteForm";
+
+export const NewNote = () => {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm />
+    </>
+  );
+};
